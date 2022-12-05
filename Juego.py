a5e7a9c6eb4c86a3c52305563fd1bc3b4f7d2225
Osvaldo Valoriani <osvaldovaloriani@hotmail.com>
2022-12-05 20:19:31 -0300

    personalizar icono de la ventana e imagenes de fondo

--- Juego.py
+++ Juego.py
@@ -5,31 +5,14 @@ pygame.init()
 
 pantalla = pygame.display.set_mode((500,400))
 
-blanco = (255,255,255)
-negro = (14,14,14)
-rojo = (215,12,12)
-azul = (0,0,190)
-verde = (0,255,0)
- 
-c74225 = (199,66,37)
-cd9135 = (97,205,53)
+pygame.display.set_caption("Exterminator")
 
-pantalla.fill(blanco)
+icono = pygame.image.load("imagenes/gunner.png")
+fondo = pygame.image.load("imagenes/ciudad.png")
 
-pygame.display.set_caption("Mi primer juego :P")
+pantalla.blit(fondo, (0,0))
+pygame.display.set_icon(icono)
 
-rectangulo1= pygame.draw.rect(pantalla, rojo, (100, 50, 100, 50))
-print(rectangulo1)
-
-pygame.draw.line(pantalla, verde, (100,104), (199,104), 10)
-
-pygame.draw.circle(pantalla, negro, (122,250),20,0)
-
-pygame.draw.ellipse(pantalla, c74225, (275, 200, 40 ,80))
-
-puntos = [(100,300), (100,100)]
-
-pygame.draw.polygon(pantalla, (0,150,255), puntos, 8)
 while True:
     for event in pygame.event.get():
       if event.type == QUIT:
