638a9c6e401165fdeae22f11b10075ed67cefe5a
Osvaldo Valoriani <osvaldovaloriani@hotmail.com>
2022-12-07 15:58:08 -0300

    movimiento de sprites con el teclado y limites de margenes

--- Juego2.py
+++ Juego2.py
@@ -10,6 +10,7 @@ blanco = (255,255,255)
 negro = (0,0,0)
 rojo = (255,0,0)
 azul = (0,0,255)
+azul2 = (64,64,255)
 verde = (0,255,0)
  
 h_fa2f2f= (250,47,47,)
@@ -18,18 +19,46 @@ h_50d2fe= (94,210,254)
 class Jugador(pygame.sprite.Sprite):
   def __init__(self):
     super().__init__()
-    self.image = pygame.image.load("imagenes/idle1.png").convert()
+    self.image = pygame.image.load("imagenes/nave.png").convert()
     
-    self.image.set_colorkey(verde)
+    self.image.set_colorkey(azul2)
 
     self.rect = self.image.get_rect()
     self.rect.center = (ancho//2, alto//2)
+    self.velocidad_x = 0 
+    self.velocidad_y = 0
 
   def update(self):
-    self.rect.x -= 10
-    if self.rect.right < 0:
-      self.rect.left = ancho
- 
+
+    self.velocidad_x = 0 
+    self.velocidad_y = 0
+
+    teclas = pygame.key.get_pressed()
+
+    if teclas[pygame.K_a]:
+      self.velocidad_x = -10
+    if teclas[pygame.K_d]:
+       self.velocidad_x = 10
+    if teclas[pygame.K_w]:
+      self.velocidad_y = -10
+    if teclas[pygame.K_s]:
+       self.velocidad_y = 10
+
+    self.rect.x += self.velocidad_x
+    self.rect.y += self.velocidad_y
+  
+    if self.rect.left < 0:
+      self.rect.left = 0
+    if self.rect.right > ancho:
+      self.rect.right= ancho
+    
+    if self.rect.bottom > alto:
+      self.rect.bottom = alto
+    
+    if self.rect.top < 0:
+      self.rect.top = 0
+
+
 pygame.init()
 pantalla = pygame.display.set_mode((ancho, alto))
 pygame.display.set_caption("Trabajando con sprites")
