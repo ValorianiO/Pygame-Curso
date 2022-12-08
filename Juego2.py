df9a3eb32f830aed6efdd7de3412dc872e6cdd4a
Osvaldo Valoriani <osvaldovaloriani@hotmail.com>
2022-12-08 16:16:33 -0300

    sistema de puntuaciones y mostrar texto en pantalla

--- Juego2.py
+++ Juego2.py
@@ -17,6 +17,12 @@ verde = (0,255,0)
 h_fa2f2f= (250,47,47,)
 h_50d2fe= (94,210,254)
 
+consolas = pygame.font.match_font('consolas')
+times = pygame.font.match_font('times')
+arial  = pygame.font.match_font('arial')
+courier = pygame.font.match_font('courier')
+
+
 class Jugador(pygame.sprite.Sprite):
   def __init__(self):
     super().__init__()
@@ -47,8 +53,6 @@ class Jugador(pygame.sprite.Sprite):
        self.velocidad_y = 10
     if teclas[pygame.K_SPACE]:
        jugador.disparo()
-       jugador.disparo2()
-       jugador.disparo3()
 
     self.rect.x += self.velocidad_x
     self.rect.y += self.velocidad_y
@@ -67,25 +71,106 @@ class Jugador(pygame.sprite.Sprite):
   def disparo(self):
     bala = Disparos(self.rect.centerx, self.rect.top)
     balas.add(bala)
-  def disparo2(self):
+  '''def disparo2(self):
     bala = Disparos(self.rect.centerx + 23, self.rect.top + 30)
     balas.add(bala)
   def disparo3(self):
     bala = Disparos(self.rect.centerx - 23, self.rect.top + 30)
-    balas.add(bala)
+    balas.add(bala)'''
 
-class Enemigos(pygame.sprite.Sprite):
+class EnemigosAmarillos(pygame.sprite.Sprite):
   def __init__(self):
     super().__init__()
-    self.image = pygame.image.load("imagenes/enemigo.png").convert()
+    self.image = pygame.image.load("imagenes/enemigo1.png").convert()
     
     self.rect = self.image.get_rect()
     self.image.set_colorkey(negro)
     self.radius = 48
     self.rect.x = random.randrange(ancho - self.rect.width)
     self.rect.y = random.randrange(alto - self.rect.height)
-    self.velocidad_x = random.randrange(1,10)
-    self.velocidad_y = random.randrange(1,10)
+    self.velocidad_x = random.randrange(1,3)
+    self.velocidad_y = random.randrange(1,3)
+
+  def  update(self):
+    self.rect.x += self.velocidad_x
+    self.rect.y += self.velocidad_y
+    if self.rect.left < 0:
+      self.velocidad_x += 1
+    if self.rect.right > ancho:
+      self.velocidad_x -= 1
+    
+    if self.rect.bottom > alto:
+      self.velocidad_y -= 1
+    
+    if self.rect.top < 0:
+      self.velocidad_y += 1
+
+class EnemigosVerdes(pygame.sprite.Sprite):
+  def __init__(self):
+    super().__init__()
+    self.image = pygame.image.load("imagenes/enemigo2.png").convert()
+    
+    self.rect = self.image.get_rect()
+    self.image.set_colorkey(negro)
+    self.radius = 48
+    self.rect.x = random.randrange(ancho - self.rect.width)
+    self.rect.y = random.randrange(alto - self.rect.height)
+    self.velocidad_x = random.randrange(3,5)
+    self.velocidad_y = random.randrange(3,5)
+
+  def  update(self):
+    self.rect.x += self.velocidad_x
+    self.rect.y += self.velocidad_y
+    if self.rect.left < 0:
+      self.velocidad_x += 1
+    if self.rect.right > ancho:
+      self.velocidad_x -= 1
+    
+    if self.rect.bottom > alto:
+      self.velocidad_y -= 1
+    
+    if self.rect.top < 0:
+      self.velocidad_y += 1
+
+class EnemigosAzules(pygame.sprite.Sprite):
+  def __init__(self):
+    super().__init__()
+    self.image = pygame.image.load("imagenes/enemigo3.png").convert()
+    
+    self.rect = self.image.get_rect()
+    self.image.set_colorkey(negro)
+    self.radius = 48
+    self.rect.x = random.randrange(ancho - self.rect.width)
+    self.rect.y = random.randrange(alto - self.rect.height)
+    self.velocidad_x = random.randrange(5,10)
+    self.velocidad_y = random.randrange(5,10)
+
+  def  update(self):
+    self.rect.x += self.velocidad_x
+    self.rect.y += self.velocidad_y
+    if self.rect.left < 0:
+      self.velocidad_x += 1
+    if self.rect.right > ancho:
+      self.velocidad_x -= 1
+    
+    if self.rect.bottom > alto:
+      self.velocidad_y -= 1
+    
+    if self.rect.top < 0:
+      self.velocidad_y += 1
+
+class EnemigosRojos(pygame.sprite.Sprite):
+  def __init__(self):
+    super().__init__()
+    self.image = pygame.image.load("imagenes/enemigo4.png").convert()
+    
+    self.rect = self.image.get_rect()
+    self.image.set_colorkey(negro)
+    self.radius = 48
+    self.rect.x = random.randrange(ancho - self.rect.width)
+    self.rect.y = random.randrange(alto - self.rect.height)
+    self.velocidad_x = random.randrange(10,15)
+    self.velocidad_y = random.randrange(10,15)
 
   def  update(self):
     self.rect.x += self.velocidad_x
@@ -141,30 +226,38 @@ class  Meteoritos(pygame.sprite.Sprite):
       self.rect.y = -self.rect.width
       self.velocidad_y = random.randrange(1,10)
 
-
-
 pygame.init()
 pantalla = pygame.display.set_mode((ancho, alto))
+
+puntuacion = 0
+
+def muestra_texto(pantalla, fuente, texto, color, dimensiones, x, y):
+  tipo_letra = pygame.font.Font(fuente, dimensiones)
+  superficie = tipo_letra.render(texto, True, color)
+  rectangulo = superficie.get_rect()
+  rectangulo.center = (x, y)
+  pantalla.blit(superficie, rectangulo)
+
+
+
 pygame.display.set_caption("Trabajando con sprites")
 clock = pygame.time.Clock()
 
 sprites =pygame.sprite.Group()
-enemigos =pygame.sprite.Group()
+enemigos_amarillos=pygame.sprite.Group()
+enemigos_verdes=pygame.sprite.Group()
+enemigos_azules=pygame.sprite.Group()
+enemigos_rojos=pygame.sprite.Group()
 balas = pygame.sprite.Group()
 meteoritos = pygame.sprite.Group()
-
-
-enemigo = Enemigos()
-enemigos.add(enemigo)
-
 
 
 jugador = Jugador()
 sprites.add(jugador)
 
-for x in range (10):
+'''for x in range (10):
   meteorito = Meteoritos()
-  meteoritos.add(meteorito)
+  meteoritos.add(meteorito)'''
 
 ejecutando = True
 
@@ -175,26 +268,55 @@ while ejecutando:
       ejecutando = False
 
   sprites.update()
-  enemigos.update()
+  enemigos_amarillos.update()
+  enemigos_azules.update()
+  enemigos_verdes.update()
+  enemigos_rojos.update()
   balas.update()
   meteoritos.update()
 
-  colision_nave = pygame.sprite.spritecollide(jugador, meteoritos, False, pygame.sprite.collide_circle)
-  colision_disparos = pygame.sprite.groupcollide(meteoritos, balas, False, False)
+  colision_disparos_amarillos = pygame.sprite.groupcollide(enemigos_amarillos, balas, True, True, pygame.sprite.collide_circle)
+  colision_disparos_verdes = pygame.sprite.groupcollide(enemigos_verdes, balas, True, True, pygame.sprite.collide_circle)
+  colision_disparos_azules = pygame.sprite.groupcollide(enemigos_azules, balas, True, True, pygame.sprite.collide_circle)
+  colision_disparos_rojos= pygame.sprite.groupcollide(enemigos_rojos, balas, True, True, pygame.sprite.collide_circle)
 
-  if colision_nave or colision_disparos:
-    enemigo.image = pygame.image.load("imagenes/explosion.png")
-    enemigo.velocidad_y += 20
-  elif enemigo.rect.top > alto:
-    enemigo.kill()
+  if colision_disparos_amarillos:
+    puntuacion += 10
 
+  if colision_disparos_verdes:
+    puntuacion += 25
 
+  if colision_disparos_azules:
+    puntuacion += 50
+
+  if colision_disparos_rojos:
+    puntuacion += 100
+
+  if not enemigos_amarillos and not enemigos_verdes and not enemigos_azules and not enemigos_rojos:
+    enemigo1 = EnemigosAmarillos()
+    enemigos_amarillos.add(enemigo1)
+
+    enemigo2 = EnemigosVerdes()
+    enemigos_verdes.add(enemigo2)
+
+    enemigo3 = EnemigosAzules()
+    enemigos_azules.add(enemigo3)
+
+    enemigo4 = EnemigosRojos()
+    enemigos_rojos.add(enemigo4)
+
+  
   pantalla.fill(negro)
   sprites.draw(pantalla)
-  enemigos.draw(pantalla)
+  enemigos_amarillos.draw(pantalla)
+  enemigos_verdes.draw(pantalla)
+  enemigos_azules.draw(pantalla)
+  enemigos_rojos.draw(pantalla)
   balas.draw(pantalla)
   meteoritos.draw(pantalla)
+  muestra_texto(pantalla, consolas, str(puntuacion), rojo, 40,700, 50)
   pygame.display.flip()
   
+
 
 pygame.quit()
